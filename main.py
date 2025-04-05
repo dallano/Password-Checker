bdb827a21a77dfe9618e1d17fdcf053410f14d36
Abdiah <62350957+dallano@users.noreply.github.com>
2025-04-05 10:23:58 -0600

    Update main.py
    Spelling error corrections

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # The following program was written to give users more security when checking for
 #   Leaked passwords. It takes in a list of passwords the user would like to check for,
 #   and creates a utf-8 hash of the password. It then sends the first 5 digites of this
-#   hash to an api. The said API then returns the remainder of the hashes that match
-#   the sendt query along with a count of how many times that password has been leaked.
-#   the program then compares that with the hash that wasn't sent and checks gives the
+#   hash to an API. The said API then returns sets of the hashes that match
+#   the sent query along with a count of how many times that password has been leaked.
+#   This program then compares that with the remaining hash that wasn't sent and reports to the
 #   user the amount of times their password has been leaked.
-#
+# 
 # In order to check for leaked passwords, the user must modify the PASSWORDS list.
-#   It is recommended that the user also delete their passwords from said list to ensure
-#   optimal security.
+#   It is recommended that the user also delete their passwords from said list after
+#   the program has finished to ensure optimal security.
 #
 #   Author: Dallan Atwood
 ## ***************************************************************************************
